699cad35ff4db9f54060ca0c1a5c4fe44b24aedb
Your Name <okoyecharles1990@gmail.com>
2023-11-15 06:49:40 -0800

    update

--- printf.c
+++ printf.c
@@ -6,8 +6,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int character_print = 0;
-	va_list argument_list;
+	int character_print = 0, va_list argument_list;
 
 	if (format == NULL)
 		return (-1);
@@ -24,24 +23,21 @@ int _printf(const char *format, ...)
 			else if (*format == '%')
 				write(1, format, 1), character_print++;
 			else if (*format == 'c')
-			{
 				int c = va_arg(argument_list, int);
 
 				write(1, &c, 1), character_print++;
-			}
 			else if (*format == 's')
 			{
-				char *str = va_arg(argument_list, char*);
-				int str_len = 0;
+				char *str = va_arg(argument_list, char*), int str_len = 0;
 
 				while (str[str_len] != '\0')
 					str_len++;
 				write(1, str, str_len), character_print += str_len;
 			}
 			else if (*format == 'd' || *format == 'i')
-				int n = va_arg (argument_list, int);
-				write(1, &n, 1 );
+				int n = va_arg(argument_list, int);
 
+				write(1, &n, 1);
 			else
 				write(1, "%", 1), write(1, format, 1), character_print += 2;
 		}
